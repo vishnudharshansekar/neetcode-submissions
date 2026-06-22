5cbe3a96a6cb2c48c17ceff54e4223e4c374848f
vishnudharshansekar <vishnudharshansekar@gmail.com>
2026-06-22 11:57:34 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        for(int x : nums){
+            mp[x]++;
+        }
+        for(int i=0;i<=n;i++){
+            if(mp[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
