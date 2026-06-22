a5850ad7a764ead73a8c7476f9da69b6a06cec5f
vishnudharshansekar <vishnudharshansekar@gmail.com>
2026-06-22 12:19:11 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int reach=0;
+        for(int i=0;i<n;i++){
+            if(i > reach)return false;
+            reach=max(reach,i+nums[i]);
+            if(reach >= n-1)return true;
+        }
+        return false;
+    }
+};
